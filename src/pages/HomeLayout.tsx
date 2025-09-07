7194df3f88b1db0f8887befd3c6135b6fa4b0210
rmsid1312 <rmsid1312@naver.com>
2025-09-07 22:46:13 +0900

    Header component

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 export default function HomeLayout() {
   return (
     <>
-      <header>header</header>
+      <Header />
       <nav>navbar</nav>
       <Outlet />
     </>
