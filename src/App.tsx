50b68efe5f0509864cb586e69e74e72e56706dc2
rmsid1312 <rmsid1312@naver.com>
2025-09-07 09:19:44 +0900

    setup shadcn/ui, tailwindcss@3

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold ">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
+}
+export default App;
