cb1e032b6ae2bae48869c24c028851e5dea5c8ce
rmsid1312 <rmsid1312@naver.com>
2025-09-07 22:07:33 +0900

    redux tool kit

---
+++ src/features/theme/themeSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "theme slice",
+};
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {},
+});
+
+export default themeSlice.reducer;
