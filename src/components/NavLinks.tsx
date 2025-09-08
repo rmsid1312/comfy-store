dd5b2ea2f84d0fdc5b92e79724721b3385a876be
rmsid1312 <rmsid1312@naver.com>
2025-09-08 23:57:22 +0900

    NavLinks 수정 isActive는 NavLink 속성 중 하나이다. className에서 함수로 파라미터를 넘길 때, 속성 중 하나를 구조분해 할당 받아서 넘기는 것이다.

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,3 +1,23 @@
+import { NavLink } from "react-router-dom";
+import { links } from "../utils";
 export default function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <div className="hidden lg:flex gap-x-4 justify-center items-center">
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? "text-blue-500" : ""
+              }`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
