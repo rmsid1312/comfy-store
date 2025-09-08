7d672ce976ece9988e52debab42d5fc54b942774
rmsid1312 <rmsid1312@naver.com>
2025-09-08 23:21:49 +0900

    Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import { Armchair } from "lucide-react";
+
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Link
+      to="/"
+      className="lg:flex hidden justify-center items-center bg-primary p-2 rounded-lg text-white"
+    >
+      <Armchair className="h-8 w-8" />
+    </Link>
+  );
 }
