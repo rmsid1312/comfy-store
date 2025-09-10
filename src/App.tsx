410ccd5c57b4ea290887d3223e7cbbea6a118ed3
rmsid1312 <rmsid1312@naver.com>
2025-09-10 23:35:03 +0900

    Error Page

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
@@ -54,10 +55,12 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: "/register",
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
