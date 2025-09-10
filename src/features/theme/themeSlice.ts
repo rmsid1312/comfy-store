b5b17464c7a4f92e7ac587a412ad11259414864d
rmsid1312 <rmsid1312@naver.com>
2025-09-10 23:06:00 +0900

    Theme Slice

--- src/features/theme/themeSlice.ts
+++ src/features/theme/themeSlice.ts
@@ -1,15 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { applyTheme } from "../../utils";
 
 export type Theme = "dark" | "light" | "system";
 
-const initialState = {
-  name: "theme slice",
+type ThemeState = {
+  theme: Theme;
+};
+
+const initializeTheme = (): Theme => {
+  const theme = (localStorage.getItem("theme") as Theme) || "system";
+  applyTheme(theme);
+  return theme;
+};
+
+const initialState: ThemeState = {
+  theme: initializeTheme(),
 };
 
 const themeSlice = createSlice({
   name: "theme",
   initialState,
-  reducers: {},
+  reducers: {
+    setTheme: (state, action: PayloadAction<Theme>) => {
+      state.theme = action.payload;
+      applyTheme(action.payload);
+      localStorage.setItem("theme", action.payload);
+    },
+  },
 });
 
 export default themeSlice.reducer;
