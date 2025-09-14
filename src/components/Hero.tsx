34bb234f5c7a497232c44e251b0db040758db5ff
rmsid1312 <rmsid1312@naver.com>
2025-09-14 16:53:46 +0900

    Hero Components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl sm:text-6xl tracking-tight">
+          We are changin the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quos
+          repellat obcaecati nisi. Obcaecati possimus temporibus, facilis esse
+          excepturi rerum repudiandae magnam unde. Pariatur tenetur obcaecati
+          dolores odio qui molestiae.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link to="products">Our Products </Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
