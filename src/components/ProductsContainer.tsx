26514d223cbecacdfc472219704e7ac57cd5bc55
rmsid1312 <rmsid1312@naver.com>
2025-09-14 17:58:50 +0900

    Product Container Component

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,9 +1,61 @@
+import { useLoaderData } from "react-router-dom";
 import ProductsList from "./ProductsList";
+import type { ProductsResponse } from "../utils";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { LayoutGrid, List } from "lucide-react";
+import { Separator } from "./ui/separator";
+import ProductGrid from "./ProductGrid";
 
 export default function ProductsContainer() {
+  const { meta } = useLoaderData() as ProductsResponse;
+  const totalProducts = meta.pagination.total;
+  const [layout, setLayout] = useState<"list" | "grid">("grid");
+
   return (
-    <div>
-      <ProductsList />
-    </div>
+    <>
+      {/* HEADER */}
+      <section>
+        <div className="flex justify-between items-center mt-8">
+          <h4 className="font-medium text-md ">
+            {totalProducts} product{totalProducts > 1 && "s"}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              onClick={() => {
+                setLayout("grid");
+              }}
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+            >
+              <LayoutGrid />
+            </Button>
+            <Button
+              onClick={() => {
+                setLayout("list");
+              }}
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+            >
+              <List />
+            </Button>
+          </div>
+        </div>
+        <Separator className="mt-4" />
+      </section>
+
+      {/* PRODUCTS */}
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className="text-2xl mt-16">
+            Sorry, no products matched you search...
+          </h5>
+        ) : layout === "grid" ? (
+          <ProductGrid />
+        ) : (
+          <ProductsList />
+        )}
+      </div>
+    </>
   );
 }
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -8,32 +8,28 @@ export default function ProductsList() {
 
   return (
     <div className="mt-12 grid gap-y-8 ">
-      <Card>
-        {products.map((product, idx) => {
-          const { image, title, price, company } = product.attributes;
-          const dollarsAmount = formatAsDollars(price);
-          return (
-            <Link to={`${product.id}`} key={idx}>
-              <Card>
-                <CardContent className="p-8 gay-y-4 grid md:grid-cols-3">
-                  <img
-                    src={image}
-                    alt={title}
-                    className="h-64 w-full md:h-48 rounded-md object-cover"
-                  />
-                  <div className="p-4">
-                    <h2 className="text-2xl font-semibold capitalize">
-                      {title}
-                    </h2>
-                    <h4>{company}</h4>
-                  </div>
-                  <p className="text-primary md:ml-auto">{dollarsAmount} </p>
-                </CardContent>
-              </Card>
-            </Link>
-          );
-        })}
-      </Card>
+      {products.map((product, idx) => {
+        const { image, title, price, company } = product.attributes;
+        const dollarsAmount = formatAsDollars(price);
+        return (
+          <Link to={`${product.id}`} key={idx}>
+            <Card>
+              <CardContent className="p-8 gay-y-4 grid md:grid-cols-3">
+                <img
+                  src={image}
+                  alt={title}
+                  className="h-64 w-full md:h-48 rounded-md object-cover"
+                />
+                <div className="p-4">
+                  <h2 className="text-2xl font-semibold capitalize">{title}</h2>
+                  <h4>{company}</h4>
+                </div>
+                <p className="text-primary md:ml-auto">{dollarsAmount} </p>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
     </div>
   );
 }
