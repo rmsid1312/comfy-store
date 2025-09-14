13c4508e95772fa916d9871672b27e8ee85e42b9
rmsid1312 <rmsid1312@naver.com>
2025-09-14 17:09:21 +0900

    Carousel Content

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,3 +1,42 @@
+import { Card, CardContent } from "./ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+
+import hero1 from "../assets/hero1.webp";
+import hero2 from "../assets/hero2.webp";
+import hero3 from "../assets/hero3.webp";
+import hero4 from "../assets/hero4.webp";
+
+const carouselImages = [hero1, hero2, hero4, hero3];
 export default function HeroCarousel() {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, idx) => {
+            return (
+              <CarouselItem key={idx}>
+                <Card>
+                  <CardContent className="p-2">
+                    <img
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
