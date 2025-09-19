3e2800d3db4b9675e322756e36b686f938ef0780
rmsid1312 <rmsid1312@naver.com>
2025-09-19 22:11:54 +0900

    Loading Component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,19 @@
+import { Skeleton } from "./ui/skeleton";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 pt-12 gap-4">
+      {Array.from({ length: 3 }).map((_, index) => {
+        return (
+          <div key={index} className="flex flex-col space-y-3">
+            <Skeleton className="h-[125px] w-full rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 mx-auto w-[250px]" />
+              <Skeleton className="h-4 mx-auto w-[250px]" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
