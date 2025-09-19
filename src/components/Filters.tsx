83dc9563cee9b0332e734375d606c92e5b5e980a
rmsid1312 <rmsid1312@naver.com>
2025-09-19 22:53:02 +0900

    Default Value - issue : api response doesn't have param field

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,9 +1,14 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useLoaderData } from "react-router-dom";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import type { ProductsResponseWithParams } from "../utils";
 
 export default function Filters() {
+  console.log(useLoaderData() as ProductsResponseWithParams);
+  const { meta, params } = useLoaderData() as ProductsResponseWithParams;
+  // const { search } = params;
+
   return (
     <Form className="border rounded-md px-8 py-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid gap-4">
       <div className="mb-2">
