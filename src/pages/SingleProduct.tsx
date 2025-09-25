24b662df159cbda4df99c57af9452dda71ea3c53
rmsid1312 <rmsid1312@naver.com>
2025-09-25 23:06:53 +0900

    Render Image and Info

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,7 +1,10 @@
-import { useLoaderData, type LoaderFunction } from "react-router-dom";
+import { Link, useLoaderData, type LoaderFunction } from "react-router-dom";
 import { customFetch, type SingleProductResponse } from "../utils";
 import { formatAsDollars } from "../utils/formatAsDollars";
 import { useState } from "react";
+import { Button } from "../components/ui/button";
+import { Separator } from "../components/ui/separator";
+import { SelectProductAmount, SelectProductColor } from "../components";
 
 export const loader: LoaderFunction = async ({ params }) => {
   const response = await customFetch<SingleProductResponse>(
@@ -23,5 +26,44 @@ export default function SingleProduct() {
   const addToCart = () => {
     console.log("add to cart");
   };
-  return <h1 className="text-4xl">SingleProduct Page</h1>;
+  return (
+    <section>
+      <div className="flex gap-x-2 h-6 items-center">
+        <Button asChild variant="link" size="sm">
+          <Link to="/">Home </Link>
+        </Button>
+        <Separator orientation="vertical" />
+        <Button asChild variant="link" size="sm">
+          <Link to="/products">Product </Link>
+        </Button>
+      </div>
+
+      {/* Product */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* Image first col */}
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 rounded-lg lg:w-full"
+        />
+        {/* Product info second col */}
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
+            {dollarsAmount}
+          </p>
+          <p className="mt-6 leading-8">{description}</p>
+          {/* Colors */}
+          <SelectProductColor />
+          {/* Amount */}
+          <SelectProductAmount />
+          {/* Cart Button */}
+          <Button size="lg" className="mt-10" onClick={addToCart}>
+            Add to bag
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
