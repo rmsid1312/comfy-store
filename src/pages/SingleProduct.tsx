e1081d6ae98c6ef7e27bc3687a8e0aa60ea0c575
rmsid1312 <rmsid1312@naver.com>
2025-09-25 22:58:22 +0900

    Fetch Single Product

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,10 +1,27 @@
-import type { LoaderFunction } from "react-router-dom";
+import { useLoaderData, type LoaderFunction } from "react-router-dom";
+import { customFetch, type SingleProductResponse } from "../utils";
+import { formatAsDollars } from "../utils/formatAsDollars";
+import { useState } from "react";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  console.log(params);
-  return null;
+  const response = await customFetch<SingleProductResponse>(
+    `/products/${params.id}`
+  );
+
+  console.log(response);
+  return { ...response.data };
 };
 
 export default function SingleProduct() {
+  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatAsDollars(price);
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const addToCart = () => {
+    console.log("add to cart");
+  };
   return <h1 className="text-4xl">SingleProduct Page</h1>;
 }
