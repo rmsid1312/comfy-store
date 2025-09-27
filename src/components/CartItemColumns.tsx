293afd4daf27d487d3e3cd510631142518a78bd0
rmsid1312 <rmsid1312@naver.com>
2025-09-27 15:57:30 +0900

    CartItems

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -1,3 +1,84 @@
-export default function CartItemColumns() {
-  return <div>CartItemColumns</div>;
-}
+import { editItems, removeItems } from "../features/cart/cartSlice";
+import { useAppDispatch } from "../hooks";
+import { formatAsDollars } from "../utils/formatAsDollars";
+import SelectProductAmount, { Mode } from "./SelectProductAmount";
+import { Button } from "./ui/button";
+
+export const FirstColumn = ({
+  image,
+  title,
+}: {
+  image: string;
+  title: string;
+}) => {
+  return (
+    <img
+      src={image}
+      alt={title}
+      className="h-24 w-24 rounded-lg sm:h-32 sm:w-32"
+    />
+  );
+};
+
+export const SecondColumn = ({
+  title,
+  company,
+  productColor,
+}: {
+  title: string;
+  company: string;
+  productColor: string;
+}) => {
+  return (
+    <div className="sm:ml-4 md:ml-12 sm:w-48">
+      <h3 className="capitalize font-semibold mb-2">{title}</h3>
+      <span className="mb-2 capitalize">{company}</span>
+      <p className="text-sm capitalize flex items-center gap-x-2">
+        color :
+        <span
+          className="rounded-full"
+          style={{
+            width: "15px",
+            height: "15px",
+            borderRadius: "50%",
+            background: productColor,
+          }}
+        ></span>
+      </p>
+    </div>
+  );
+};
+
+export const ThirdColumn = ({
+  amount,
+  cartID,
+}: {
+  amount: number;
+  cartID: string;
+}) => {
+  const dispatch = useAppDispatch();
+  const removeItemFromCart = () => {
+    dispatch(removeItems(cartID));
+  };
+
+  const setAmount = (value: number) => {
+    dispatch(editItems({ cartID, amount: value }));
+  };
+
+  return (
+    <div>
+      <SelectProductAmount
+        amount={amount}
+        setAmount={setAmount}
+        mode={Mode.CartItem}
+      />
+      <Button variant="link" className="-ml-4" onClick={removeItemFromCart}>
+        remove
+      </Button>
+    </div>
+  );
+};
+
+export const FourthColumn = ({ price }: { price: string }) => {
+  return <p className="ml-auto font-medium">{formatAsDollars(price)}</p>;
+};
--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -1,3 +1,35 @@
+import { useAppSelector } from "../hooks";
+import {
+  FirstColumn,
+  FourthColumn,
+  SecondColumn,
+  ThirdColumn,
+} from "./CartItemColumns";
+import { Card } from "./ui/card";
+
 export default function CartItemList() {
-  return <div>CartItemList</div>;
+  const cartItems = useAppSelector((state) => state.cartState.cartItems);
+  return (
+    <div>
+      {cartItems.map((item) => {
+        const { cartID, title, price, image, amount, company, productColor } =
+          item;
+        return (
+          <Card
+            key={cartID}
+            className="flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8"
+          >
+            <FirstColumn image={image} title={title} />
+            <SecondColumn
+              title={title}
+              company={company}
+              productColor={productColor}
+            />
+            <ThirdColumn amount={amount} cartID={cartID} />
+            <FourthColumn price={price} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
