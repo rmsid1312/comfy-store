ecdd9886623a54c72e3a0b5efe87b9f51986f927
rmsid1312 <rmsid1312@naver.com>
2025-09-27 16:04:56 +0900

    User slice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,13 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { toast } from "../../hooks/use-toast";
 
-const initialState = {
-  name: "user slice",
+export type User = { username: string; jwt: string };
+export type UserState = { user: User | null };
+const getUserFromLocalStorage = () => {
+  const user = localStorage.getItem("user");
+  if (!user) return null;
+  return JSON.parse(user);
+};
+
+const initialState: UserState = {
+  user: getUserFromLocalStorage(),
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, action: PayloadAction<User>) => {
+      const user = action.payload;
+      state.user = user;
+      localStorage.setItem("user", JSON.stringify(user));
+      if (user.username === "demo user") {
+        toast({ description: "Welcome Guest Users" });
+        return;
+      }
+      toast({ description: "Login successful" });
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      localStorage.removeItem("user");
+    },
+  },
 });
 
 export default userSlice.reducer;
+export const { loginUser, logoutUser } = userSlice.actions;
