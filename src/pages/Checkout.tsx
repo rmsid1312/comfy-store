74b1838c7c76e8f9e88c3db3c4c61bc9f5759894
rmsid1312 <rmsid1312@naver.com>
2025-09-28 18:51:53 +0900

    Checkout Page Loader - Restrict unlogin user to access the checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,12 +1,17 @@
-import type { LoaderFunction } from "react-router-dom";
+import { redirect, type LoaderFunction } from "react-router-dom";
 import { CartTotals, CheckoutForm, SectionTitle } from "../components";
 import { useAppSelector } from "../hooks";
 import type { ReduxStore } from "../store";
+import { toast } from "../hooks/use-toast";
 
 export const loader =
   (store: ReduxStore): LoaderFunction =>
-  async (): Promise<null> => {
-    console.log(store);
+  async (): Promise<null | Response> => {
+    const user = store.getState().userState.user;
+    if (!user) {
+      toast({ description: "Please login to continue" });
+      return redirect("/login");
+    }
     return null;
   };
 
