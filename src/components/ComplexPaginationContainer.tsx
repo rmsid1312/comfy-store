04e39fa4dee819ac54240408b523e11db16c6427
rmsid1312 <rmsid1312@naver.com>
2025-09-28 19:44:14 +0900

    Complex pagination

--- src/components/ComplexPaginationContainer.tsx
+++ src/components/ComplexPaginationContainer.tsx
@@ -1,5 +1,113 @@
-import React from "react";
+import { useLoaderData, useLocation } from "react-router-dom";
+import {
+  constructPrevOrNextUrl,
+  constructUrl,
+  type OrdersResponse,
+  type ProductsResponseWithParams,
+} from "../utils";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
 
 export default function ComplexPaginationContainer() {
-  return <div>ComplexPaginationContainer</div>;
+  const { meta } = useLoaderData() as OrdersResponse;
+  const { pageCount, page } = meta.pagination;
+  const { search, pathname } = useLocation();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+  console.log(pages);
+
+  if (pageCount < 2) return null;
+
+  // const renderPagination = pages.map((pageNumber) => {
+  //   const isActive = pageNumber === page;
+  //   const url = constructUrl({ pageNumber, search, pathname });
+  //   return (
+  //     <PaginationItem key={pageNumber}>
+  //       <PaginationLink to={url} isActive={isActive}>
+  //         {pageNumber}
+  //       </PaginationLink>
+  //     </PaginationItem>
+  //   );
+  // });
+
+  const constructButton = ({
+    pageNumber,
+    isActive,
+  }: {
+    pageNumber: number;
+    isActive: boolean;
+  }): React.ReactNode => {
+    const url = constructUrl({ pageNumber, search, pathname });
+    return (
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  };
+
+  const constructEllipsis = (key: string): React.ReactNode => {
+    return (
+      <PaginationItem key={key}>
+        <PaginationEllipsis />
+      </PaginationItem>
+    );
+  };
+
+  const renderPagination = () => {
+    let pages: React.ReactNode[] = [];
+
+    // first page
+    pages.push(constructButton({ pageNumber: 1, isActive: page === 1 }));
+
+    //ellipsis
+    if (page > 2) {
+      pages.push(constructEllipsis("dots-1"));
+    }
+
+    //active page
+    if (page !== 1 && page !== pageCount) {
+      pages.push(constructButton({ pageNumber: page, isActive: true }));
+    }
+
+    //ellipsis
+    if (page < pageCount - 1) {
+      pages.push(constructEllipsis("dots-1"));
+    }
+
+    // last page
+    pages.push(
+      constructButton({ pageNumber: pageCount, isActive: page === pageCount })
+    );
+    return pages;
+  };
+
+  const { prevUrl, nextUrl } = constructPrevOrNextUrl({
+    currentPage: page,
+    pageCount,
+    search,
+    pathname,
+  });
+
+  return (
+    <Pagination className="mt-16">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious to={prevUrl} />
+        </PaginationItem>
+        {renderPagination()}
+        <PaginationItem>
+          <PaginationNext to={nextUrl} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 }
