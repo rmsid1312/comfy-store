7b65663a0617d4bea24ef278a0239172dc67588f
rmsid1312 <rmsid1312@naver.com>
2025-09-28 17:56:30 +0900

    Login Page Setup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,32 @@
+import { Form, Link } from "react-router-dom";
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
+import { FormInput, SubmitBtn } from "../components";
+import { Button } from "../components/ui/button";
+
 export default function Login() {
-  return <h1 className="text-4xl">Login Page</h1>;
+  const loginAsGuestUser = () => {
+    console.log('Guest User')
+  }
+  return <section className="h-screen grid place-items-center">
+    <Card className="w-96">
+      <CardHeader>
+        <CardTitle className="text-center">Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form method="post">
+          <FormInput type="email" label='email' name="identifier" /> 
+          <FormInput type="password"  name="password" /> 
+          <SubmitBtn text="Login" className="w-full mt-4" />
+          <Button type='button' variant='outline' onClick={loginAsGuestUser} className="w-full mt-4">
+            Guest User</Button> 
+        </Form>
+        <p className="mt-4 text-center">
+              Not a member yet ?{" "}
+              <Button asChild variant="link" type="button">
+                <Link to="/register">Register</Link>
+              </Button>
+            </p>
+      </CardContent>
+    </Card>
+  </section>;
 }
